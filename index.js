de97bf0258b03ee6ff6509a31f2ca270b0233359
Diego de los Reyes <diegorys@gmail.com>
2019-11-18 18:40:34 +0100

    LED

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const five = require("johnny-five");
 
 console.log('Hi Johny Five from diegorys.es');
 
-let board = five.Board();
+const board = five.Board();
+board.on("ready", () => {
+    console.log('The board is ready!');
+    const led = new Led(13);
+    led.blink();
+    console.log('The led is blinking!');
+});
